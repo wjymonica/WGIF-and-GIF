df85006750dc285610415d6c0c35e2eba72b3859
Jingying Wang <wangchy@umich.edu>
2021-04-19 11:36:50 +0800

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -1,29 +1,40 @@
+include("gif.jl")
+include("rgif.jl")
+include("wgif.jl")
+include("sdfilter.jl")
+include("ag_sdfilter.jl")
+using Noise
+using MIRTjim:jim
+using Plots
 using Images, FileIO
 using Statistics
 using TestImages
 using ImageBinarization
 using LinearAlgebra
-using Plots
-include("applications.jl")
-function rgb2mat_256(path)
-    img = load(path)
-    img = channelview(img) .*255
-    return img
-end
+using ImageMagick
 
-r = 60
-e = 7
-ratio = 0.98
-omega = 31/32
-lambda = 1/1000
-haze_level = 0.03125
+nei=5;                # 0: 4-neighbor 1: 8-neighbor
+lambda = 10;           # smoothness parameter
+mu = 500;              # bw for static guidance
+nu = 200;              # bw for dynamic guidance
+step=20;
+issp = true;
+#m, n, = size(img)
 
-img = load("WGIF-Julia/img/tulips.bmp")
-img = Float64.(channelview(img))
 
-#img[1, :, :] = wgif_m(img[1, :, :], img[1, :, :], 10, 1/1000)
-#img[2, :, :] = wgif_m(img[2, :, :], img[2, :, :], 10, 1/1000)
-#img[3, :, :] = wgif_m(img[3, :, :], img[3, :, :], 10, 1/1000)
-G1 = detail_enhancement(img, img, 16, 1/128, 4, "wgif")
-#G2 = dehazing(img, "gif")
-colorview(RGB, G1)
+img =  ImageMagick.load("/Users/wangjingying/Desktop/EECS556/sdfilter/imgs/depthSR/books.bmp")
+
+img = Float64.(Gray.(img))
+noisy_image = add_gauss(img, 0.1)
+u0 = ones(size(noisy_image))
+colorview(Gray, img)
+
+out_gif = gif(noisy_image, noisy_image, 10, 1/10)
+out_wgif = wgif(noisy_image, noisy_image, 10, 1/10)
+out_wgif1 = wgif_1(noisy_image, noisy_image, 10, 1/10)
+out_wgif2 = wgif_2(noisy_image, noisy_image, 10, 1/10)
+out_wgif3 = wgif_3(noisy_image, noisy_image, 10, 1/10)
+out_wgif4 = wgif_4(noisy_image, noisy_image, 10, 1/10)
+out_sd = sdfilter(noisy_image,u0,noisy_image,nei,lambda,mu,nu,20,issp)
+out_asd = asd(noisy_image,u0,noisy_image,nei,lambda,mu,nu,20,issp)
+out_agsd = agsd(noisy_image,u0,noisy_image,nei,lambda,mu,nu,20,issp)
